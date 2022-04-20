467f82915bb02099ea1ad3e2907ac06cae1049a7
Kelly Jefferies <k.jefferies@gmail.com>
2022-04-20 17:39:03 -0500

    Require Update

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { Post } = require("../../models");
+const { Post } = require("../../models/index");
 // const sequelize = require("../../config/connection");
 const withAuth = require('../../utils/auth');
 
