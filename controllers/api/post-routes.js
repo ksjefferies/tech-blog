69977a329ffaf11a5d7d7334ac2e1f9cc96c1723
Kelly Jefferies <k.jefferies@gmail.com>
2022-04-20 17:51:48 -0500

    re-add index

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { Post } = require("../../models/Index");
+const { Post } = require("../../models/");
 // const sequelize = require("../../config/connection");
 const withAuth = require('../../utils/auth');
 
